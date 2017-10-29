40e554fd665e76af384403c9318d7b8bd87f6010
Shiva1029 <shiva007charan@yahoo.co.in>
2017-10-28 22:35:42 -0400

    add time display

--- src/app/custom-lib/time-ago.ts
+++ src/app/custom-lib/time-ago.ts
@@ -1,7 +1,7 @@
 export const timeAgo = function (date) {
-    let d = new Date();
-    let UTCsecondsNow = (d.getTime() + d.getTimezoneOffset() * 60 * 1000);
-    let UTCseconds = (date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+    const d = new Date();
+    const UTCsecondsNow = (d.getTime() + d.getTimezoneOffset() * 60 * 1000);
+    const UTCseconds = (date.getTime() + date.getTimezoneOffset() * 60 * 1000);
     let diff = UTCsecondsNow - UTCseconds;
     let tense = 'ago';
     if (diff < 0) {
@@ -12,33 +12,33 @@ export const timeAgo = function (date) {
         return 'Just Now';
     }
     // 365.25 * 24 * 60 * 60 * 1000
-    let years = singular(Math.round(diff / 31557600000), 'Year');
+    const years = singular(Math.round(diff / 31557600000), 'Year');
     if (years) {
         return years + tense;
     }
-    let months = singular(Math.round(diff / 2592000000), 'Month');
+    const months = singular(Math.round(diff / 2592000000), 'Month');
     if (months) {
         return months + tense;
     }
-    let days = singular(Math.round(diff / 86400000), 'Day');
+    const days = singular(Math.round(diff / 86400000), 'Day');
     if (days) {
         return days + tense;
     }
-    let hours = singular(Math.round(diff / 3600000), 'Hour');
+    const hours = singular(Math.round(diff / 3600000), 'Hour');
     if (hours) {
         return hours + tense;
     }
-    let mins = singular(Math.round(diff / 60000), 'Minute');
+    const mins = singular(Math.round(diff / 60000), 'Minute');
     if (mins) {
         return mins + tense;
     }
-    let secs = singular(Math.round(diff / 1000), 'Second');
+    const secs = singular(Math.round(diff / 1000), 'Second');
     if (secs) {
         return secs + tense;
     }
 };
 
-let singular = function (num, str) {
+const singular = function (num, str) {
     if (num > 1) {
         if (num === 1) {
             return '1 ' + str + ' ';
